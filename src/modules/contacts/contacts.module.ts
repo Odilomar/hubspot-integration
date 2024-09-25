4c59925f1cac855feaaea1bc62a0386c1c16e3a2
Odilomar <BRSSI0007@totvspartners.com.br>
2024-09-25 20:37:25 -0300

    fix: update contacts module

--- src/modules/contacts/contacts.module.ts
+++ src/modules/contacts/contacts.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { GetContactsUseCaseService } from './use-cases';
+import { CreateContactUseCase, GetContactsUseCaseService } from './use-cases';
 import { HubspotModule } from '../hubspot/hubspot.module';
 import { ContactsController } from './controllers';
 
 @Module({
   imports: [HubspotModule],
-  providers: [GetContactsUseCaseService],
+  providers: [GetContactsUseCaseService, CreateContactUseCase],
   controllers: [ContactsController],
 })
 export class ContactsModule {}
