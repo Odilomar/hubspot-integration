a714c6021414a6b1b6576dd929a6ce3f8b997724
Odilomar <BRSSI0007@totvspartners.com.br>
2024-09-25 19:47:09 -0300

    fix use .env

--- src/modules/hubspot/services/base-hubspot-api.service.ts
+++ src/modules/hubspot/services/base-hubspot-api.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Client as HubspotApiClient } from '@hubspot/api-client';
+import { configuration } from '../../../config';
 
+const {
+  hubspot: { accessToken },
+} = configuration;
 @Injectable()
 export class BaseHubspotApiService {
   hubspotApiClient: HubspotApiClient;
 
   constructor() {
-    this.hubspotApiClient = new HubspotApiClient({ apiKey: '' });
+    this.hubspotApiClient = new HubspotApiClient({
+      accessToken,
+    });
   }
 }
