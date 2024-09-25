94c919055a2d344c774782e2a1b42082a56fc09d
Odilomar <BRSSI0007@totvspartners.com.br>
2024-09-25 20:37:08 -0300

    feat: create contacts controller

--- src/modules/contacts/controllers/contacts.controller.ts
+++ src/modules/contacts/controllers/contacts.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { GetContactsUseCaseService } from '../use-cases';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { CreateContactUseCase, GetContactsUseCaseService } from '../use-cases';
 import { ApiTags } from '@nestjs/swagger';
-import { GetContactsDto } from '../dtos';
+import { CreateContactDto, GetContactsDto } from '../dtos';
 
 @ApiTags('Contacts')
-@Controller('Contacts')
+@Controller('contacts')
 export class ContactsController {
   constructor(
     private readonly getContactsUseCaseService: GetContactsUseCaseService,
+    private readonly createContactUseCase: CreateContactUseCase,
   ) {}
 
   @Get('/')
   getContacts(@Query() query: GetContactsDto) {
     return this.getContactsUseCaseService.execute(query);
+  }
+
+  @Post('/')
+  createContact(@Body() body: CreateContactDto) {
+    return this.createContactUseCase.execute(body);
   }
 }
