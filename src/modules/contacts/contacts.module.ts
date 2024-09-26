e1a56e4f6e3fa0a34b667181234400489df8e6bf
Odilomar <BRSSI0007@totvspartners.com.br>
2024-09-25 21:35:31 -0300

    feat: add use case to module

--- src/modules/contacts/contacts.module.ts
+++ src/modules/contacts/contacts.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { CreateContactUseCase, GetContactsUseCase } from './use-cases';
 import { HubspotModule } from '../hubspot/hubspot.module';
 import { ContactsController } from './controllers';
+import { CreateContactsInBatchUseCase } from './use-cases/create-contacts-in-batch.use-case';
 
 @Module({
   imports: [HubspotModule],
-  providers: [GetContactsUseCase, CreateContactUseCase],
+  providers: [
+    GetContactsUseCase,
+    CreateContactUseCase,
+    CreateContactsInBatchUseCase,
+  ],
   controllers: [ContactsController],
 })
 export class ContactsModule {}
